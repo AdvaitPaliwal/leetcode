1ce43f4765ac33885397d0035431604ca5a87d01
Advait Paliwal <advaitspaliwal@gmail.com>
2022-08-15 14:43:40 +0530

    initial commit

---
+++ Easy/001_Two_Sum.py
@@ -0,0 +1,20 @@
+'''
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    You can return the answer in any order.
+'''
+
+def twoSum(nums, target):
+    d = {}
+    for i,n in enumerate(nums):
+        match = target - n
+        if match in d:
+            return d[match], i
+        else:
+            d[n] = i
+
+nums = [2,7,11,15]
+target = 26
+print(twoSum(nums, target))
